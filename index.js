5c3c3c8619ce9155802b1e07b142de1035ba4f87
Sannu Sharma <sannusharma02@gmail.com>
2025-03-07 12:39:49 +0530

    dog api

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
+
+    superagent
+        .get(`https://dog.ceo/api/breed/hound/images/random`)
+        .end((err,res) => {
+        console.log(res.body);
+        if (err) return console.log(err.message);
+        fs.writeFile('dog-img.txt', res.body, 'utf8', (err) => {
+            console.log('Random dog image save');
+        });
+    });
 });
 
